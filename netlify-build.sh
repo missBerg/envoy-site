7aa97018b44bd069baf4384e26ed3f74ce0d5523
Erica Hughberg <erica.sundberg.90@gmail.com>
2025-01-06 13:43:51 -0500

    Update netlify-build.sh

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,1 +1,11 @@
 bundle exec jekyll build
+
+git clone --depth=1 https://github.com/envoyproxy/envoy.git "envoy"
+git config --global --add safe.directory /envoy 
+
+cd envoy/docs
+bazel run --//tools/tarball:target=//docs:html //tools/tarball:unpack /generated/docs
+
+cd ..
+
+cp -rf /docs/generated/docs/* _site/docs/envoy
