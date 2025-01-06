b9c3599d51e9ff353c6050954049172c02c66348
Erica Hughberg <erica.sundberg.90@gmail.com>
2025-01-06 14:16:42 -0500

    Update netlify-build.sh

--- netlify-build.sh
+++ netlify-build.sh
@@ -3,19 +3,19 @@ bundle exec jekyll build
 git clone --depth=1 https://github.com/envoyproxy/envoy.git "envoy"
 git config --global --add safe.directory /envoy 
 
-mkdir -p /tools/bazelisk
+ks -lart
 
-curl -L -o /tools/bazelisk https://github.com/bazelbuild/bazelisk/releases/download/v1.17.0/bazelisk-linux-amd64 \
-    && chmod +x /tools/bazelisk \
-    && ln -s /tools/bazelisk /tools/bazel
+curl -L -o $BAZELISK_HOME https://github.com/bazelbuild/bazelisk/releases/download/v1.17.0/bazelisk-linux-amd64 \
+    && chmod +x $BAZELISK_HOME \
+    && ln -s $BAZELISK_HOME /tools/bazel
 
 echo $PWD
 
-export PATH="$PWD/tools:$PATH"
+export PATH="$BAZELISK_HOME:$PATH"
 
 cd envoy/docs
-bazel run --//tools/tarball:target=//docs:html //tools/tarball:unpack /generated/docs
+bazel run --//tools/tarball:target=//docs:html //tools/tarball:unpack ./generated/docs
 
 cd ..
 
-cp -rf /docs/generated/docs/* _site/docs/envoy
+cp -rf ./docs/generated/docs/* ./_site/docs/envoy
