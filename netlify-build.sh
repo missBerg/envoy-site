63140816b18a43d521e371fd8568c93fe18b7bc8
Erica Hughberg <erica.sundberg.90@gmail.com>
2025-01-06 14:24:09 -0500

    Update netlify-build.sh

--- netlify-build.sh
+++ netlify-build.sh
@@ -5,17 +5,17 @@ git config --global --add safe.directory /envoy
 
 ks -lart
 
-curl -L -o $BAZELISK_HOME https://github.com/bazelbuild/bazelisk/releases/download/v1.17.0/bazelisk-linux-amd64 \
-    && chmod +x $BAZELISK_HOME \
-    && ln -s $BAZELISK_HOME /tools/bazel
+curl -L -o $BAZELISK_HOME https://github.com/bazelbuild/bazelisk/releases/download/v1.17.0/bazelisk-linux-amd64
+chmod +x $BAZELISK_HOME \
+ln -s .$BAZELISK_HOME /opt/build/cache/bazel
 
 echo $PWD
 
-export PATH="$BAZELISK_HOME:$PATH"
+export PATH="$PWD/tools:$PATH"
 
 cd envoy/docs
-bazel run --//tools/tarball:target=//docs:html //tools/tarball:unpack ./generated/docs
+bazel run --//tools/tarball:target=//docs:html //tools/tarball:unpack /opt/build/cache/generated/docs
 
 cd ..
 
-cp -rf ./docs/generated/docs/* ./_site/docs/envoy
+cp -rf /opt/build/cache/generated/docs/* /opt/build/site/_site/docs/envoy
