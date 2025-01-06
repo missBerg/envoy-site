f5adfe46a3a99b3aff472d6199676f6bb02da14b
Erica Hughberg <erica.sundberg.90@gmail.com>
2025-01-06 11:21:13 -0500

    trying netlify build

---
+++ netlify-build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e  # Exit on error
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+#--------------------------------------
+# Source Functions
+#--------------------------------------
+
+source ./scripts/functions/logging.sh
+source ./scripts/functions/build_proxy_docs_latest.sh
+source ./scripts/functions/ruby_setup.sh
+source ./scripts/functions/jekyll.sh
+source ./scripts/functions/get_proxy_docs_archive.sh
+
+#--------------------------------------
+
+
+#--------------------------------------
+# Main Script
+#--------------------------------------
+
+bundle exec jekyll build
+
+#--------------------------------------
