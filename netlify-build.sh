c5a5b89ddfd9980cfc2ef723a6527e4107c770b9
Erica Hughberg <erica.sundberg.90@gmail.com>
2025-01-07 12:37:06 -0500

    Update netlify-build.sh

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,24 +1,3 @@
-bundle exec jekyll build
-
-git clone --depth=1 https://github.com/envoyproxy/envoy.git "envoy-source"
-git config --global --add safe.directory /envoy 
-
-ls -lart
-
-# cd envoy-source/docs
-# bazel build -j 12 --local_ram_resources=HOST_RAM*.8 //docs:html_release
-
-cd envoy-source
-
-build_docs
-
-
-ls -lart
-
-# cd ..
-
-# cp -rf /opt/build/cache/generated/docs/* /opt/build/repo/_site/docs/envoy
-
 function build_docs {
 
     BAZEL_STARTUP_OPTIONS=(
@@ -49,3 +28,25 @@ function build_docs {
             "$DOCS_OUTPUT_DIR"
 
 }
+
+bundle exec jekyll build
+
+git clone --depth=1 https://github.com/envoyproxy/envoy.git "envoy-source"
+git config --global --add safe.directory /envoy 
+
+ls -lart
+
+# cd envoy-source/docs
+# bazel build -j 12 --local_ram_resources=HOST_RAM*.8 //docs:html_release
+
+cd envoy-source
+
+ls -lart
+
+build_docs
+
+ls -lart
+
+# cd ..
+
+# cp -rf /opt/build/cache/generated/docs/* /opt/build/repo/_site/docs/envoy
