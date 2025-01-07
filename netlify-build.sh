3977d968d6a38c8f4603b23bcba5c2c6d885fbd1
Erica Hughberg <erica.sundberg.90@gmail.com>
2025-01-07 14:15:59 -0500

    Update netlify-build.sh

--- netlify-build.sh
+++ netlify-build.sh
@@ -8,6 +8,7 @@ function build_docs {
     BAZEL_STARTUP_OPTIONS=(
     "--output_user_root=${BUILD_DIR}/bazel_root"
     "--output_base=${BUILD_DIR}/bazel_root/base"
+    "--host_jvm_args=-Xmx2g --host_jvm_args=-Xms1g"
     )
 
     echo "generating docs..."
@@ -17,8 +18,8 @@ function build_docs {
     rm -rf "${DOCS_OUTPUT_DIR}"
     mkdir -p "${DOCS_OUTPUT_DIR}"
     # SPHINX_ARGS="-j 12 -v warn"
-    # export SPHINX_RUNNER_ARGS="-j 12 "
-    BAZEL_BUILD_OPTIONS+=("--host_jvm_args=-Xmx2g --host_jvm_args=-Xms1g")
+    # export SPHINX_RUNNER_ARGS="-j 12"
+    # BAZEL_BUILD_OPTIONS+=("--action_env=SPHINX_RUNNER_ARGS=\"${SPHINX_RUNNER_ARGS}\" --host_jvm_args=-Xmx2g --host_jvm_args=-Xms1g")
 
     # if [[ -n "${DOCS_BUILD_RST}" ]]; then
     #     bazel "${BAZEL_STARTUP_OPTIONS[@]}" build "${BAZEL_BUILD_OPTIONS[@]}" //docs:rst
