cc36ba2f67d1927ffe737aae7809aaa857bca3d7
Erica Hughberg <erica.sundberg.90@gmail.com>
2025-01-07 22:05:11 -0500

    cleaning up scripts and local setup

--- scripts/sync-site.sh
+++
@@ -1,8 +0,0 @@
-source /home/builder/envoy-site/scripts/functions/logging.sh
-apk add --no-cache inotify-tools rsync &&
-
-info "Syncing site contents..." "jekyll_start"
-while true; do
-    inotifywait -r -e modify,create,delete,move /home/builder/envoy-site &&
-    rsync -ah --info=progress2 --no-perms --no-owner --no-group --delete --exclude='docs' --exclude='_site' --exclude='.git' /home/builder/app/
-done
