72b9e082887b07db446783a57fb8331060412a23
Erica Hughberg <erica.sundberg.90@gmail.com>
2025-01-08 22:41:46 -0500

    Update jekyll.sh

--- scripts/functions/jekyll.sh
+++ scripts/functions/jekyll.sh
@@ -7,20 +7,3 @@ jekyll_start (){
         info "$line" "jekyll_start"
     done
 }
-
-
-jekyll_build (){
-    log_function_start "${GREEN}" "jekyll_build"
-
-    # Setup Ruby
-    ruby_setup
-
-    # Install Ruby Gems
-    gem_install
-
-    info "Starting Jekyll Server..." "jekyll_build"
-    
-    bundle exec jekyll build --trace -s /home/builder/app -d /home/builder/generated 2>&1 | while IFS= read -r line; do
-        info "$line" "jekyll_build"
-    done
-}
